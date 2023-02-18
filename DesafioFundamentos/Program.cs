5da519a9045911a6c5afc4561e11d499f534943d
vinicius <vmm_7@hotmail.com>
2023-02-18 16:34:31 -0300

    simplificando try catch

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -6,23 +6,12 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem-vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-// precoInicial = Convert.ToDecimal(Console.ReadLine());
 try
 {
+    Console.WriteLine("Seja bem-vindo ao sistema de estacionamento!\n" + "Digite o preço inicial:");
     precoInicial = Convert.ToDecimal(Console.ReadLine());
-}
-catch
-{
-    Console.WriteLine("Opção inválida :(\nEncerraremos o programa.");
-    Environment.Exit(0);
-}
 
-Console.WriteLine("Agora digite o preço por hora:");
-// precoPorHora = Convert.ToDecimal(Console.ReadLine());
-try
-{
+    Console.WriteLine("Agora digite o preço por hora:");
     precoPorHora = Convert.ToDecimal(Console.ReadLine());
 }
 catch
